d099f1215065fecf5c388217b3f4dc3473dc848d
Paulo Victor <paulovmel@gmail.com>
2021-09-12 19:37:56 -0300

    feat: adds TransactionsTable component

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => console.log(response.data));
+  }, []);
+
+  
+  return (
+    <Container>
+      <table>
+        <thead>
+          <th>Título</th>
+          <th>Preço</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td className="deposit">R$ 12.000,00</td>
+            <td>Venda</td>
+            <td>13/04/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel do apartamento</td>
+            <td className="withdraw">- R$ 1.200,00</td>
+            <td>Casa</td>
+            <td>27/03/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
